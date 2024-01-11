5adbcaff7afd61152f2e093bec00bcb5cd52a3db
mwszekely <19625841+mwszekely@users.noreply.github.com>
2024-01-11 07:48:59 -0500

    Type fix

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,14 +1,22 @@
 import { FilterPattern } from "@rollup/pluginutils";
 import { InputPluginOption } from "rollup";
-export interface DataPluginOptions {
+export interface ChunkWorkersPluginOptions {
     /**
-     * Files prefixed with `datafile:` will always be included, but this can be used to load files even if they don't.
+     * Standard Rollup `include` pattern; applies to the string in `new URL("./worker.js")`.
      */
     include?: FilterPattern;
     /**
-     * Excludes any files that were included by `include` (has no effect on `datafile:` ids)
+     * Standard Rollup `exclude` pattern; applies to the string in `new URL("./worker.js")`.
      */
     exclude?: FilterPattern;
+    /**
+     * If provided, allows renaming a file to be different from whatever it was imported as.
+     *
+     * This is particularly useful for changing the directory something is in once built.
+     *
+     * E.G. `p => path.parse(p).base` will place everything in the build's root directory (with `import * as path from "node:path"`).
+     */
+    transformPath?: (normalizedPath: string) => string;
 }
-export default function chunkWorkersPlugin({ exclude, include }?: Partial<DataPluginOptions>): InputPluginOption;
+export default function chunkWorkersPlugin({ exclude, include, transformPath }?: Partial<ChunkWorkersPluginOptions>): InputPluginOption;
 export { chunkWorkersPlugin };
